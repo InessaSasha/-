c6baa15de7bec2a99e51ae3a81b9d675fce6aab2
InessaSasha <sashagetcourse@gmail.com>
2024-07-27 17:54:39 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,12 @@
+def divide(first, second):
+
+    if second == 0:
+        return ('Ошибка')
+    else:
+        return first / second
+
+
+res1 = divide(69, 3)
+print(res1)
+res2 = divide(3, 0)
+print(res2)
