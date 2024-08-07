9b49cf09d5642bba5ba61ece6b6fa8ad0dc328c0
InessaSasha <sashagetcourse@gmail.com>
2024-08-07 17:10:32 +0300

    Add files via upload

---
+++ module_5_3.py.py
@@ -0,0 +1,71 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        self.number_of_floors += value
+        return self
+
+
+# h1 = House('ЖК Горский', 18)
+# h2 = House('Домик в деревне', 2)
+# h1.go_to(5)
+# h2.go_to(10)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2, '- метод eq')  # __eq__
+
+h1 = h1 + 10  # __add__
+print(h1)
+print(h1 == h2, '- метод add')
+
+h1 += 10  # __iadd__
+print(h1, '- метод iadd')
+
+h2 = 10 + h2  # __radd__
+print(h2, '- метод radd')
+
+print(h1 > h2, '- метод gt')  # __gt__
+print(h1 >= h2, '- метод ge')  # __ge__
+print(h1 < h2, '- метод lt')  # __lt__
+print(h1 <= h2, '- метод le')  # __le__
+print(h1 != h2, '- метод ne')  # __ne__
